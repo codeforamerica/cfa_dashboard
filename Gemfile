064ec5bf52a170a9b2e59198d69be1bc2b3c9d4f
Moncef Belyamani <giter@done>
2013-02-03 15:12:41 -0800

    added latest CfA GitHub commits to fun_facts dashboard

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source :rubygems
 gem 'dashing'
 gem 'nokogiri'
 gem 'twitter'
+gem "github_api"
---
+++ jobs/github.rb
@@ -0,0 +1,42 @@
+require "net/http"
+require "github_api"
+
+Github.new ssl: { verify: false }
+
+SCHEDULER.every '10m', :first_in => 0 do |job|
+
+	activities = []
+
+	cfa = Github.activity.events.org 'codeforamerica'
+
+	cfa.each do |event| 
+		actor = event["actor"]["login"]
+		type = event["type"]
+		repo = event["repo"]["name"]
+
+		if type == "PushEvent" 
+			activity = "pushed to"
+			branch = event["payload"]["ref"].split("refs/heads/")[1]
+			message = event["payload"]["commits"].first["message"]
+			url = repo
+			activities.push({name: actor, activity: activity, subject: branch, preposition: "at", url: "#{url}:", body: message})
+
+		elsif (type == "IssuesEvent" or type == "IssueCommentEvent")
+		 	action = event["payload"]["action"]
+			if action == "opened"
+				activity = "opened issue"
+			elsif action == "closed"
+				activity = "closed issue"
+			end
+			if type == "IssueCommentEvent"
+				activity = "commented on issue"
+			end
+			number = event["payload"]["issue"]["number"]
+			issue = "#{repo}##{number}"
+			message = event["payload"]["issue"]["title"]
+			event = "#{type} #{issue}:"
+			activities.push({name: actor, activity: activity, subject: "#{issue}:", body: message})
+		end
+	end
+	send_event('cfa_github', comments: activities)
+end
